cbac4ca61023eecf8dfd3263c26ff0b42e1a18dc
Scott Wolff <scott.wolff@tuscan.ca>
2010-10-29 09:57:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkJournal::Application.config.secret_token = '810b78c389e541c5b67bf12bfd3bfa14698c0c649932c68a86bcecaf283a6c428100db20cad645df001333c6e36b8506d89aff98370d337329309b7f1b2f0611'
---
+++ readme.md
@@ -0,0 +1,1 @@
+This RAILS3 applicaiton template was created by Scott Wolff (@rswolff), and can be forked on github at http://github.com/rswolff/rails3-app.
